790366e9a9fcb33f2e64391dd736f8479456691d
devshakti78 <shakti.dev46@gmail.com>
2022-04-16 20:43:58 +0530

    home done

--- src/Components/AddCountry.jsx
+++ src/Components/AddCountry.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
-import {
-  CountryLoading,
-  CountrySuccess,
-  CountryFailed,
-  UpdateDetails,
-} from "../Redux/action";
+import { UpdateDetails } from "../Redux/action";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export const AddCountry = () => {
   const [country, setCountry] = useState("");
   const [city, setCity] = useState("");
   const [population, setPopulation] = useState("");
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
+    alert("Country Added Successfully");
     const details = {
       country_name: country,
       city_name: city,
       population: population,
     };
-
     dispatch(UpdateDetails(details));
   };
+
+  //   const HandleContinue = () => {
+  //     console.log("country", country);
+  //     navigate("/add-city", <AddCity country_name={country} />);
+  //   };
 
   return (
     <div>
@@ -59,3 +61,39 @@ export const AddCountry = () => {
     </div>
   );
 };
+
+{
+  /* <div>
+      <input
+        type="text"
+        placeholder="Country Name"
+        value={country}
+        onChange={(e) => {
+          setCountry(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="City Name"
+        value={city}
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="Total Population"
+        value={population}
+        onChange={(e) => {
+          setPopulation(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+    </div> */
+}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [data, setData] = useState([]);
+  const [sortdata, setSortdata] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/Country`)
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .then((res) => setSortdata(res))
+      .catch((res) => console.log(err));
+  }, []);
+
+  //   console.log("data", data);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <button>Sort by Population</button>
+      <br />
+      <br />
+      <table style={{ border: "1.5px solid black" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid gray" }}>S.No</th>
+            <th style={{ border: "1px solid gray" }}>Country</th>
+            <th style={{ border: "1px solid gray" }}>City</th>
+            <th style={{ border: "1px solid gray" }}>Population</th>
+            <th style={{ border: "1px solid gray" }}>Update</th>
+            <th style={{ border: "1px solid gray" }}>Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((e) => (
+            <tr key={e.id}>
+              <td style={{ border: "1px solid gray" }}>{e.id}.</td>
+              <td style={{ border: "1px solid gray" }}>{e.country_name}</td>
+              <td style={{ border: "1px solid gray" }}>{e.city_name}</td>
+              <td style={{ border: "1px solid gray" }}>{e.population}</td>
+              <td>
+                <button>Edit</button>
+              </td>
+              <td>
+                <button
+                //   onClick={(e) => {
+                //     data.filter((e) => setData())
+                //   }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
